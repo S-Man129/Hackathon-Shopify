e72d4e35bbb10d2cb5c33007cba9db0340006393
S-Man129 <adebayosulayman129@gmail.com>
2023-11-25 11:32:27 +0100

    file uploaded

---
+++ app.js
@@ -0,0 +1,9 @@
+const cardTitle = document.getElementsByClassName("card-content-title");
+
+cardTitle.addEventListener('click', function(){
+    console.log('You clicked me');
+})
+
+const iconCollapse = document.querySelector("#icon-arrow-up");
+
+    
